6b4e964053e812763b2769a7f45c915862b851f8
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2018-10-07 15:59:13 +0200

    build framework for the artwork details page

--- client/src/views/schilderijDetails/SchilderijDetails.js
+++ client/src/views/schilderijDetails/SchilderijDetails.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './SchilderijDetails.css'
 
 // Components
-import PageTitle from '../../components/pageLink/PageLink'
+import Tabs from '../../components/tabs/Tabs';
 
 class SchilderijDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageData: []
+      imageData: [],
+      current: 0,
+      horizontal: false,
+      swipe: true,
+      wheel: true,
+      animate: true,
+      factor: 0.3,
+      loop: true,
+      index: 0
     }
+  }
+
+  change(event) {
+    let target = event.target;
+    let index = Array.prototype.indexOf.call(target.parentElement.children, target);
+
+    this.setState({
+      current: index
+    });
   }
 
   componentDidMount() {
@@ -18,9 +35,49 @@ class SchilderijDetails extends Component {
   }
 
   render() {
+    var tabData = [
+      {
+        title: "Tab 1",
+        content: "Content tab 1"
+      },
+      {
+        title: "Tab 2",
+        content: "Content tab 2"
+      },
+      {
+        title: "Tab 3",
+        content: "Content tab 3"
+      }
+    ]
     return (
       <section className="section-container">
-        {/* <PageTitle title={this.state.imageData.id}/> */}
+        {/* Image and details container */}
+        <div className="flex row-nowrap">
+          <div className="image-container flex center">
+          </div>
+          <div className="details-container flex column-nowrap y-center">
+            <span className="details-title">{this.state.imageData.title || "TITLE"}</span>
+            <span className="details-author">{this.state.imageData.author || "AUTHOR"}</span>
+            <span className="details-price">{this.state.imageData.price || "€ PRICE"}</span>
+            <div className="details-buttons flex row-nowrap">
+              <button>Bestel nu</button>
+              <button>Huren</button>
+            </div>
+            <span className="divider my-3"></span>
+            <span className="details-info">Gratis levering</span>
+            <span className="details-info">100 dagen retourrecht</span>
+          </div>
+        </div>
+        {/* Info and "people also bought" container */}
+        <div className="flex row-nowrap mt-5">
+          <div className="info-container">
+            <Tabs
+              data={tabData}
+            />
+          </div>
+          <div className="more-container">
+          </div>
+        </div>
       </section>
     );
   }
