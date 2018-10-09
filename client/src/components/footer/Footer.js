7ae241a11327e6cab1f2417160a0e6578361d7b3
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2018-10-09 12:04:33 +0200

    Updated footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,25 +6,24 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <div className="y-center">
-          <span className="mt-4">Wij versturen met</span>
-        </div>
-        <span className="y-divider"></span>
-        <div className="y-center">
-          <span className="mt-4">Betaalwijzen</span>
-        </div>
-        <span className="y-divider"></span>
-        <div className="y-center">
-          <span className="mt-4">Makkelijk online shoppen</span>
-          <ul>
-            <li>Gratis levering</li>
-            <li>Gartis retour</li>
-            <li>100 dagen retourrecht</li>
-          </ul>
-        </div>
-        <span className="y-divider"></span>
-        <div className="y-center">
-          <span className="mt-4">Verified service</span>
+        <div className="footer-container">
+          <div className="x-center">
+            <span className="mt-4">Wij versturen met</span>
+          </div>
+          <div className="x-center">
+            <span className="mt-4">Betaalwijzen</span>
+          </div>
+          <div className="x-center">
+            <span className="mt-4">Makkelijk online shoppen</span>
+            <ul>
+              <li>Gratis levering</li>
+              <li>Gartis retour</li>
+              <li>100 dagen retourrecht</li>
+            </ul>
+          </div>
+          <div className="x-center">
+            <span className="mt-4">Verified service</span>
+          </div>
         </div>
       </footer>
     );
