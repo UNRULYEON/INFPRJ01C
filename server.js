2cd253ee716e4cb64ea50c8d16825ac7472c3d27
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2018-10-09 14:59:41 +0200

    Added limit to query

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var pgp = require('pg-promise')(/*options*/)
 var db = pgp('postgres://projectc:pc@188.166.94.83:5432/project_dev')
 
 app.get('/collection', (req, res) => {
-  db.many('SELECT * from schilderijen')
+  db.many('SELECT * from schilderijen limit 15')
   .then(function (data) {
     res.send(data)
   })
