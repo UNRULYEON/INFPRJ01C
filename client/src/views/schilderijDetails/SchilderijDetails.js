a1647ab9977a2a2da6eb9a405f7e8ecfaec6e2c5
Doritosxl <doritosvslays@gmail.com>
2018-10-10 10:45:36 +0200

    Added 2 tabs in schilderijDetails. Added schilder in server

--- client/src/views/schilderijDetails/SchilderijDetails.js
+++ client/src/views/schilderijDetails/SchilderijDetails.js
@@ -7,95 +7,104 @@ import Loading from '../../components/loading/Loading'
 import Tabs from '../../components/tabs/Tabs';
 
 class SchilderijDetails extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imageData: [],
-      current: 0,
-      horizontal: false,
-      swipe: true,
-      wheel: true,
-      animate: true,
-      factor: 0.3,
-      loop: true,
-      index: 0
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageData: [],
+            current: 0,
+            horizontal: false,
+            swipe: true,
+            wheel: true,
+            animate: true,
+            factor: 0.3,
+            loop: true,
+            index: 0
+        }
     }
-  }
 
-  change(event) {
-    let target = event.target;
-    let index = Array.prototype.indexOf.call(target.parentElement.children, target);
+    change(event) {
+        let target = event.target;
+        let index = Array.prototype.indexOf.call(target.parentElement.children, target);
 
-    this.setState({
-      current: index
-    });
-  }
-
-  componentDidMount() {
-    // Get data from database
-    console.log(this.props.match.params.id)
-    fetch('/schilderij/' + this.props.match.params.id)
-      .then(res => res.json())
-      .then(res => {
-        console.log(res)
         this.setState({
-          imageData: res
-        })
-      })
-    // setState(data from database)
-  }
+            current: index
+        });
+    }
 
-  render() {
-    var tabData = [
-      {
-        title: "Beschrijving",
-        content: this.state.imageData.description
-      }
-    ]
-    return (
-      <section className="section-container">
-        {/* Image and details container */}
-        <div className="flex row-nowrap">
-          <div className="image-container flex center">
-            <Img
-              src={[
-                this.state.imageData.bigsrc
-              ]}
-              width={
-                this.state.imageData.width
-              }
-              key={this.state.imageData.id}
-              loader={<Loading size={100} borderSize={10}/>}
-              unloader={<div className="error">It seems there was a problem<br/>loading this image.<br/>Reload the page</div>}
-            />
-          </div>
-          <div className="details-container flex column-nowrap y-center">
-            <span className="details-title">{this.state.imageData.title || "TITLE"}</span>
-            <span className="details-author">{this.state.imageData.principalmaker || "AUTHOR"}</span>
-            <span className="details-price">{this.state.imageData.price || "€ PRICE"}</span>
-            <div className="details-buttons flex row-nowrap">
-              <button>Bestel nu</button>
-              <button>Huren</button>
-            </div>
-            <span className="divider my-3"></span>
-            <span className="details-info">Gratis levering</span>
-            <span className="details-info">100 dagen retourrecht</span>
-          </div>
-        </div>
-        {/* Info and "people also bought" container */}
-        <div className="flex row-nowrap mt-5">
-          <div className="info-container">
-            <Tabs
-              data={tabData}
-              key={this.state.imageData.id}
-            />
-          </div>
-          <div className="more-container">
-          </div>
-        </div>
-      </section>
-    );
-  }
+    componentDidMount() {
+        // Get data from database
+        console.log(this.props.match.params.id)
+        fetch('/schilderij/' + this.props.match.params.id)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    imageData: res
+                })
+            })
+        // setState(data from database)
+    }
+
+    render() {
+        var tabData = [
+            {
+                title: "Beschrijving",
+                content: this.state.imageData.description
+            },
+            {
+                title: "Datum",
+                content: "Jaar uitgebracht: " + this.state.imageData.releasedate
+            },
+            {
+                title: "Materiaal",
+                content: this.state.imageData.physicalmedium
+            }
+
+        ]
+        return (
+            <section className="section-container">
+                {/* Image and details container */}
+                <div className="flex row-nowrap">
+                    <div className="image-container flex center">
+                        <Img
+                            src={[
+                                this.state.imageData.bigsrc
+                            ]}
+                            width={
+                                this.state.imageData.width
+                            }
+                            key={this.state.imageData.id}
+                            loader={<Loading size={100} borderSize={10} />}
+                            unloader={<div className="error">It seems there was a problem<br />loading this image.<br />Reload the page</div>}
+                        />
+                    </div>
+                    <div className="details-container flex column-nowrap y-center">
+                        <span className="details-title">{this.state.imageData.title || "TITLE"}</span>
+                        <span className="details-author">{this.state.imageData.principalmaker || "AUTHOR"}</span>
+                        <span className="details-price">{this.state.imageData.price || "€ PRICE"}</span>
+                        <div className="details-buttons flex row-nowrap">
+                            <button>Bestel nu</button>
+                            <button>Huren</button>
+                        </div>
+                        <span className="divider my-3"></span>
+                        <span className="details-info">Gratis levering</span>
+                        <span className="details-info">100 dagen retourrecht</span>
+                    </div>
+                </div>
+                {/* Info and "people also bought" container */}
+                <div className="flex row-nowrap mt-5">
+                    <div className="info-container">
+                        <Tabs
+                            data={tabData}
+                            key={this.state.imageData.id}
+                        />
+                    </div>
+                    <div className="more-container">
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default SchilderijDetails;
--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var db = pgp('postgres://projectc:pc@188.166.94.83:5432/project_dev')
 
 app.get('/collection', (req, res) => {
   db.many('SELECT * from schilderijen limit 15')
-  .then(function (data) {
-    res.send(data)
-  })
-  .catch(function (error) {
-    console.log('ERROR:', error)
-  })
+    .then(function (data) {
+      res.send(data)
+    })
+    .catch(function (error) {
+      console.log('ERROR:', error)
+    })
 });
 
 app.get('/schilderij/:id', (req, res) => {
@@ -20,12 +20,22 @@ app.get('/schilderij/:id', (req, res) => {
   let id = req.params.id;
 
   db.one('SELECT * from schilderijen where id = $1', [id]) //query om id te vergelijken
-  .then(function (data) {
-    res.send(data)
-  })
-  .catch(function (error) {
-    console.log('ERROR:', error)
-  })
+    .then(function (data) {
+      res.send(data)
+    })
+    .catch(function (error) {
+      console.log('ERROR:', error)
+    })
+});
+
+app.get('/schilder', (req, res) => {
+  db.many('SELECT * from schilder limit 15')
+    .then(function (data) {
+      res.send(data)
+    })
+    .catch(function (error) {
+      console.log('ERROR:', error)
+    })
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
