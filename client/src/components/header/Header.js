fd7bf6b83c2ad21d91d634b8de944aeb131342d2
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2018-10-14 14:09:45 +0200

    Added dropdown to account and cart, removed fullsize searchbar and log in button, created search field

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -6,6 +6,10 @@ import {
 import posed from "react-pose";
 import './Header.css';
 
+// Components
+import CartMenu from '../cartmenu/CartMenu';
+import AccountMenu from '../accountmenu/AccountMenu';
+
 // Icons
 import logo from '../../icons/logo.svg';
 import cart from '../../icons/cart.svg';
@@ -41,16 +45,38 @@ class Header extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			searchBar: false
+			searchBar: false,
+			accountMenuToggle: false,
+			cartMenuToggle: false
 		};
 		this.toggleSearchBar = this.toggleSearchBar.bind(this);
+		this.toggleAccount = this.toggleAccount.bind(this);
+		this.toggleCart = this.toggleCart.bind(this);
 	}
 
 	toggleSearchBar() {
 		this.setState(state => ({
-			searchBar: !state.searchBar
+			searchBar: !state.searchBar,
+			cartMenuToggle: false,
+			accountMenuToggle: false
 		}))
 		console.log("Search bar is now: " + !this.state.searchBar);
+	}
+
+	toggleAccount() {
+		this.setState(state => ({
+			accountMenuToggle: !state.accountMenuToggle,
+			cartMenuToggle: false
+		}))
+		console.log("Account menu is now: " + !this.state.accountMenuToggle);
+	}
+
+	toggleCart() {
+		this.setState(state => ({
+			cartMenuToggle: !state.cartMenuToggle,
+			accountMenuToggle: false
+		}))
+		console.log("Cart menu is now: " + !this.state.cartMenuToggle);
 	}
 
 	render() {
@@ -68,17 +94,21 @@ class Header extends Component {
 						<Link to="/"><img src={logo} alt="Logo" height="32" /></Link>
 						<Link to="/">ARTIC</Link>
 					</div>
-					<div id="header-searchbar">
-						<input id="search-bar-input" placeholder="Waar bent u naar opzoek?"></input>
-					</div>
+					<div className="spacer"></div>
 					<div id="header-actions">
 						<button onClick={this.toggleSearchBar} className="pointer header-button mr-2" id="search-icon"><img src={search} alt="Search" width="32" /></button>
-						<button className="pointer header-button mr-2" id="account-icon"><img src={account} alt="Search" width="32" /></button>
-						<button className="flex x-center mr-2 pointer header-button" id="account-btn-big">
-								Inloggen
-								<img src={arrow_down} alt="ad" width="24" />
-						</button>
-						<button className="pointer header-button"><img src={cart} alt="Cart" width="32" /></button>
+						<div className="header-dropdownmenu-container">
+							<button onClick={this.toggleAccount} className="pointer header-button mr-2" id="account-icon"><img src={account} alt="Search" width="32" /></button>
+							<AccountMenu
+								menu={this.state.accountMenuToggle}
+							/>
+						</div>
+						<div className="header-dropdownmenu-container">
+							<button onClick={this.toggleCart} className="pointer header-button"><img src={cart} alt="Cart" width="32" /></button>
+							<CartMenu
+								menu={this.state.cartMenuToggle}
+							/>
+						</div>
 					</div>
 				</div>
 				<div id="header-container-secondary">
