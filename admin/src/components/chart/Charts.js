9c1725b91759414da1e3644e2e42d16cf4bfde77
Hakan Taskirmaz <doritosvslays@gmail.com>
2018-12-19 14:38:46 +0100

    minor changes in chart

--- admin/src/components/chart/Charts.js
+++ admin/src/components/chart/Charts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Bar, Line } from 'react-chartjs-2';
+import { Bar, Line, Radar, Doughnut, Pie, Polar, Bubble, Scatter } from 'react-chartjs-2';
 
 class Chart extends Component {
   constructor(props) {
--- admin/src/views/dashboard/Dashboard.js
+++ admin/src/views/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './Dashboard.css';
 import Charts from '../../components/chart/Charts.js'
 import Button from '@material-ui/core/Button';
+import { red } from '@material-ui/core/colors';
+import color from '@material-ui/core/colors/green';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -44,6 +46,15 @@ class Dashboard extends Component {
               6,
               10
             ],
+            backgroundColor:[
+              'red',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
           }
         ],
       }
@@ -53,10 +64,10 @@ class Dashboard extends Component {
   render() {
     return (
       <section>
-        <Button onClick={this.toggleBar.bind(this)}>show the bar</Button>
+        <Button onClick={this.toggleBar.bind(this)}>Balk voorbeeld</Button>
         <div id="chart"><Charts chartData={this.state.chartData} showBar={this.state.showBar} about="Aantal bezoekers per dag" legendPosition="top" /></div>
 
-        <Button onClick={this.toggleLine.bind(this)}>show the line</Button>
+        <Button onClick={this.toggleLine.bind(this)}>Lijn voorbeeld</Button>
         <div id="chart"><Charts chartData={this.state.chartData} showLine={this.state.showLine} about="Aantal bezoekers per dag" legendPosition="top" /></div>
       </section>
     );
